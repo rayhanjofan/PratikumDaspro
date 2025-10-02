9b18e9704f32b0af87c183ff285f49e6381e3a30
rayhanjofan <rayhanjofan21@gmail.com>
2025-10-02 23:12:33 +0700

    first

--- jobsheet5/ifCetakKRS24.java
+++ jobsheet5/ifCetakKRS24.java
@@ -10,10 +10,6 @@ public class ifCetakKRS24 {
          if (uktLunas) {
              System.out.println("Pembayaran UKT terverifikasi");
              System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-
-         } else {
-             System.out.println("Pembayaran UKT belum terverifikasi");
-             
          }
     }
     
