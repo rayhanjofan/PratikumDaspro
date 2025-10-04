9bf7f914b0fcfe9e53e3e71f4aa7fa9f92124df9
rayhanjofan <rayhanjofan21@gmail.com>
2025-10-04 16:45:14 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS24.java
+++ jobsheet5/ifCetakKRS24.java
@@ -7,12 +7,8 @@ public class ifCetakKRS24 {
        System.out.println("Apakah UKT sudah lunas? (true/false)");
          boolean uktLunas = sc.nextBoolean();
 
-         if (uktLunas) {
-             System.out.println("Pembayaran UKT terverifikasi");
-             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-
-            } else {
-             System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-            }
+             String status = uktLunas ? "Pembayaran UKT terverifikasi.\nSilahkan cetak KRS dan minta tanda tangan DPA." : "Pembayaran UKT belum lunas, KRS tidak bisa dicetak.";
+        System.out.println(status);
     }   
-}
+    }   
+
