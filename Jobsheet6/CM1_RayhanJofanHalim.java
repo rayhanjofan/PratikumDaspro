bde5413b939c15e72731e3f07017858eb7a6948c
rayhanjofan <rayhanjofan21@gmail.com>
2025-10-23 11:28:17 +0700

    CM1

--- Jobsheet6/CM1_RayhanJofanHalim.java
+++ Jobsheet6/CM1_RayhanJofanHalim.java
@@ -11,7 +11,7 @@ public class CM1_RayhanJofanHalim {
         double nilaiTugas1, nilaiUTS1, nilaiUAS1, nilaiAkhir1;
         double nilaiTugas2, nilaiUTS2, nilaiUAS2, nilaiAkhir2;
         String nilaiHuruf1, nilaiHuruf2;
-        String status2, status1;
+        String status1, status2;
 
 
         System.out.println("=== INPUT DATA MAHASISWA ===");
@@ -33,30 +33,30 @@ public class CM1_RayhanJofanHalim {
         nilaiUTS2 = sc.nextDouble();
         System.out.print("Nilai UAS: ");
         nilaiUAS2 = sc.nextDouble();
-        System.out.print("Nilai Tugas: ");
+        System.out.print("Nilai Tugas: "); 
         nilaiTugas2 = sc.nextDouble();
 
 
         nilaiAkhir1 = (0.3 * nilaiTugas1) + (0.3 * nilaiUTS1) + (0.4 * nilaiUAS1);
-        if (nilaiAkhir1 >= 80 && nilaiAkhir1 <= 100) {
+        if (nilaiAkhir1 > 80 && nilaiAkhir1 <= 100) {
             nilaiHuruf1 = "A";
             status1 = "Lulus";
-        } else if (nilaiAkhir1 >= 73 && nilaiAkhir1 < 80) {
+        } else if (nilaiAkhir1 > 73 && nilaiAkhir1 <= 80) {
             nilaiHuruf1 = "B+";
             status1 = "Lulus";
-        } else if (nilaiAkhir1 >= 65 && nilaiAkhir1 < 73) {
+        } else if (nilaiAkhir1 >= 65 && nilaiAkhir1 <= 73) {
             nilaiHuruf1 = "B";
             status1 = "Lulus";
-        } else if (nilaiAkhir1 >= 60 && nilaiAkhir1 < 65) {
+        } else if (nilaiAkhir1 > 60 && nilaiAkhir1 <= 65) {
             nilaiHuruf1 = "C+";
             status1 = "Lulus";
-        } else if (nilaiAkhir1 >= 50 && nilaiAkhir1 < 60) {
+        } else if (nilaiAkhir1 > 50 && nilaiAkhir1 <= 60) {
             nilaiHuruf1 = "C";
             status1 = "Lulus";
-        } else if (nilaiAkhir1 >= 39 && nilaiAkhir1 < 50) {
+        } else if (nilaiAkhir1 > 39 && nilaiAkhir1 <= 50) {
             nilaiHuruf1 = "D";
             status1 = "Tidak Lulus";
-        } else if (nilaiAkhir1 >= 0 && nilaiAkhir1 < 39) {
+        } else if (nilaiAkhir1 >= 39) {
             nilaiHuruf1 = "E";
             status1 = "Tidak Lulus";
         } else {
@@ -66,25 +66,25 @@ public class CM1_RayhanJofanHalim {
             
         }
         nilaiAkhir2 = (0.3 * nilaiTugas2) + (0.3 * nilaiUTS2) + (0.4 * nilaiUAS2);
-        if (nilaiAkhir2 >= 80 && nilaiAkhir2 <= 100) {
+        if (nilaiAkhir2 > 80 && nilaiAkhir2 <= 100) {
             nilaiHuruf2 = "A";
             status2 = "Lulus";
-        } else if (nilaiAkhir2 >= 73 && nilaiAkhir2 < 80) {
+        } else if (nilaiAkhir2 > 73 && nilaiAkhir2 <= 80) {
             nilaiHuruf2 = "B+";
             status2 = "Lulus";
-        } else if (nilaiAkhir2 >= 65 && nilaiAkhir2 < 73) {
+        } else if (nilaiAkhir2 >= 65 && nilaiAkhir2 <= 73) {
             nilaiHuruf2 = "B";
             status2 = "Lulus";
-        } else if (nilaiAkhir2 >= 60 && nilaiAkhir2 < 65) {
+        } else if (nilaiAkhir2 > 60 && nilaiAkhir2 <= 65) {
             nilaiHuruf2 = "C+";
             status2 = "Lulus";
-        } else if (nilaiAkhir2 >= 50 && nilaiAkhir2 < 60) {
+        } else if (nilaiAkhir2 > 50 && nilaiAkhir2 <= 60) {
             nilaiHuruf2 = "C";
             status2 = "Lulus";
-        } else if (nilaiAkhir2 >= 39 && nilaiAkhir2 < 50) {
+        } else if (nilaiAkhir2 > 39 && nilaiAkhir2 <= 50) {
             nilaiHuruf2 = "D";
             status2 = "Tidak Lulus";
-        } else if (nilaiAkhir2 >= 0 && nilaiAkhir2 < 39) {
+        } else if (nilaiAkhir2 <= 39) {
             nilaiHuruf2 = "E";
             status2 = "Tidak Lulus";
         } else {
@@ -98,16 +98,36 @@ public class CM1_RayhanJofanHalim {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + NIM);
         System.out.println("Kelas: " + kelas);
+
         System.out.println("Mata Kuliah\t\t\tUTS\tUAS\tTugas\tNilai Akhir\tNilai Huruf\tStatus");
         System.out.println("------------------------------------------------------------------------------------");
+
         System.out.printf("Algoritma Dan Pemrograman\t%d\t%d\t%d\t%.2f\t\t%s\t\t%s\n", (int)nilaiUTS1, (int)nilaiUAS1, (int)nilaiTugas1, nilaiAkhir1, nilaiHuruf1, status1);
         System.out.printf("Struktur Data\t\t\t%d\t%d\t%d\t%.2f\t\t%s\t\t%s\n", (int)nilaiUTS2, (int)nilaiUAS2, (int)nilaiTugas2, nilaiAkhir2, nilaiHuruf2, status2);
+    
+
         System.out.printf("Rata-rata Nilai Akhir: %.2f\n", rataRata);
-         if (rataRata >= 70) {
+    
+    if (rataRata >= 70) {
             System.out.println("Status Semester: Lulus");
         } else {
-            System.out.println("Status Semester: TIDAK LULUS (Rata rata < 70)");
+            System.out.println("Status Semester: TIDAK LULUS");
         }
 
+        if (status1.equals("Lulus") && status2.equals("Lulus")) {
+        } else if (rataRata >= 70) {
+            System.out.println("Status Semester: Lulus");
+        } else {
+            System.out.println("Status Semester: TIDAK LULUS");
+        }
+
+        if (rataRata >= 85) {
+            System.out.println("Cumlaude: berhak beasiswa");
+        } else if (rataRata >= 70 && rataRata < 85) {
+            System.out.println("Baik: tidak berhak beasiswa");
+        } else {
+            System.out.println("Perlu perbaikan: tidak berhak beasiswa");
+            
+        }
     }
 }
