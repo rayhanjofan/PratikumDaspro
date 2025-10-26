cdf6784f8f899996947815fce5ad248b52d214f5
rayhanjofan <rayhanjofan21@gmail.com>
2025-10-26 21:23:39 +0700

    Modifikasi Percobaan 1

--- Jobsheet7/SiakadFor24.java
+++ Jobsheet7/SiakadFor24.java
@@ -6,7 +6,7 @@ public class SiakadFor24 {
 
         java.util.Scanner sc = new java.util.Scanner(System.in);
 
-        double nilai, tertinggi = 0, terendah = 100;
+        double nilai, tertinggi = 0, terendah = 100, lulus = 0, tidakLulus = 0;
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai ke-" + i + ": ");
             nilai = sc.nextDouble();
@@ -16,9 +16,18 @@ public class SiakadFor24 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                System.out.println("Status: Lulus");
+                lulus++;
+            } else {
+                System.out.println("Status: Tidak Lulus");
+                tidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi adalah: " +  tertinggi);
         System.out.println("Nilai terendah adalah: " +  terendah);
+        System.out.println("Jumlah siswa lulus: " + (int) lulus);
+        System.out.println("Jumlah siswa tidak lulus: " + (int) tidakLulus);
     }
 }
 
