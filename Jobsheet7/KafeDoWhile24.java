6997518d715f277fd3bd5d2b134e8e3841f5eaaf
rayhanjofan <rayhanjofan21@gmail.com>
2025-10-26 21:10:52 +0700

    Percobaan 3

---
+++ Jobsheet7/KafeDoWhile24.java
@@ -0,0 +1,39 @@
+package Jobsheet7;
+
+public class KafeDoWhile24 {
+
+    public static void main(String[] args) {
+
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int kopi, teh, roti;
+        String namaPelanggan;
+        int hargaKopi = 12000;
+        int hargaTeh = 7000;
+        int hargaRoti = 20000;
+
+        do {
+            System.out.print("Masukkan nama pelanggan (ketik 'batal' untuk keluar): ");
+            namaPelanggan = sc.nextLine();
+
+            if (namaPelanggan.equalsIgnoreCase("batal")) {
+                System.out.println("Program dibatalkan.");
+                break;
+            }
+
+            System.out.print("Jumlah kopi: ");
+            kopi = sc.nextInt();
+
+            System.out.print("Jumlah teh: ");
+            teh = sc.nextInt();
+
+            System.out.print("Jumlah roti: ");
+            roti = sc.nextInt();
+
+            int totalHarga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
+            System.out.println("Total yang harus dibayar: Rp " + totalHarga);
+            sc.nextLine(); // Clear the buffer
+           
+        } while (true);
+    }
+}
+
