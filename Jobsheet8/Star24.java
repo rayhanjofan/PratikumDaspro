e398e8c446d2dfbab12a4b9f3acc0249ee420d26
rayhanjofan <rayhanjofan21@gmail.com>
2025-11-05 21:50:22 +0700

    Percobaan 1

---
+++ Jobsheet8/Star24.java
@@ -0,0 +1,13 @@
+package Jobsheet8;
+
+public class Star24 {
+    public static void main(String[] args) {
+        
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.print("Masukan nilai n = ");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.print("*");
+        }
+    }
+} 
