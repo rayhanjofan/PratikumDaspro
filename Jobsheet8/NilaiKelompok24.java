b678ff8c9432c5d4d4cc0f042ee48b60cb12635e
rayhanjofan <rayhanjofan21@gmail.com>
2025-11-05 22:26:23 +0700

    Modifikasi Percobaan 4

--- Jobsheet8/NilaiKelompok24.java
+++ Jobsheet8/NilaiKelompok24.java
@@ -4,6 +4,8 @@ public class NilaiKelompok24 {
     public static void main(String[] args) {
        java.util.Scanner scanner = new java.util.Scanner(System.in);
        int nilai, j, i;
+       int kelompokTertinggi = 0;
+       float rataTertinggi = 0;
        float totalNilai, rataNilai;
        
        i = 1;
@@ -17,8 +19,12 @@ public class NilaiKelompok24 {
            }
            rataNilai = totalNilai / 5;
            System.out.println("Rata-rata Nilai Kelompok " + i + " : " + rataNilai);
+           if (rataNilai > rataTertinggi) {
+                rataTertinggi = rataNilai;
+                kelompokTertinggi = i;
+           }
            i++;
        }
+       System.out.println("Kelompok " + kelompokTertinggi + " adalah Kelompok dengan Rata-rata Nilai Tertinggi : " + rataTertinggi);
    }
 }
-
