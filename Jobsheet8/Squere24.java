7ea455555d9bee1f9479eab078dd2d803db2694c
rayhanjofan <rayhanjofan21@gmail.com>
2025-11-06 16:24:25 +0700

    Percobaan 2

--- Jobsheet8/Squere24.java
+++ Jobsheet8/Squere24.java
@@ -6,11 +6,11 @@ public class Squere24 {
         java.util.Scanner scanner = new java.util.Scanner(System.in);
         System.out.print("Masukan nilai n = ");
         int n = scanner.nextInt();
+        for (int iOuter = 1; iOuter <= n; iOuter++) {
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
                 System.out.print("*");
-            }
-            System.out.println();
         }
+        System.out.println();
+    }
     }
 }
