6b67be285a27d305440af35a3f7783948ac1efc2
rayhanjofan <rayhanjofan21@gmail.com>
2025-11-06 22:37:13 +0700

    Modifikasi Percobaan 2

--- Jobsheet8/Squere24.java
+++ Jobsheet8/Squere24.java
@@ -7,7 +7,7 @@ public class Squere24 {
         System.out.print("Masukan nilai n = ");
         int n = scanner.nextInt();
         for (int iOuter = 1; iOuter <= n; iOuter++) {
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
                 System.out.print("*");
         }
         System.out.println();
