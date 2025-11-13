187e03c8c1d5ba8947ea973a3e1095f331b3d3c6
rayhanjofan <rayhanjofan21@gmail.com>
2025-11-13 16:53:16 +0700

    Modifikasi Percobaan 2

--- Jobsheet9/ArrayNilai24.java
+++ Jobsheet9/ArrayNilai24.java
@@ -3,13 +3,20 @@ package Jobsheet9;
 public class ArrayNilai24 {
     public static void main(String[] args) {
         java.util.Scanner sc = new java.util.Scanner(System.in);
+
         int[] nilaiAkhir = new int [10];
-        for (int i = 0; i < 10; i++) {
+
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.println("Masukan nilai akhir ke- " + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
 }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke- " + i + " : " + nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70){
+                System.out.println("Mahasiswa ke-" + i + " Lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-" + i + " Tidak Lulus!");
+            }
         }
+
     }
 }
