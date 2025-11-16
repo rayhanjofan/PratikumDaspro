ab1974b3cdfc5c588575399575e761c90554af29
rayhanjofan <rayhanjofan21@gmail.com>
2025-11-16 20:04:08 +0700

    Modifikasi Percobaan 1

--- Jobsheet10/Bioskop24.java
+++ Jobsheet10/Bioskop24.java
@@ -21,5 +21,14 @@ public class Bioskop24 {
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
 
+        System.out.println(penonton.length);
+        
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
+        for (int i = 0; i < penonton.length; i++){
+            System.out.println("Penonton pada baris ke- " + (i+1) + ": " + String.join(", ", penonton[i]));
+        }
     }
 }
+        
