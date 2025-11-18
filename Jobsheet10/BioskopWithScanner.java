ee08d548051c20658debec3cfe5570311dd9da73
rayhanjofan <rayhanjofan21@gmail.com>
2025-11-18 22:01:56 +0700

    Percobaan 2

---
+++ Jobsheet10/BioskopWithScanner.java
@@ -0,0 +1,36 @@
+package Jobsheet10;
+
+import java.util.Scanner;
+
+public class BioskopWithScanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[][] penonton = new String[4][2];
+
+        String nama, next;
+        int baris, kolom;
+
+        while (true) {
+            System.out.print("Masukkan nama: ");
+            nama = sc.nextLine();
+            
+            System.out.print("Masukkan baris: ");
+            baris = sc.nextInt();
+            
+            System.out.print("Masukkan kolom: ");
+            kolom = sc.nextInt();
+            sc.nextLine(); // buang newline
+
+            penonton[baris-1][kolom-1] = nama;
+
+            System.out.print("Input penonton lainnya? (y/n): ");
+            next = sc.nextLine();
+
+            if (next.equalsIgnoreCase("n")) {
+                break;
+            }
+        }
+    }
+}
+
