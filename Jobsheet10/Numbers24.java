f2a1b36b384a2e30b083a4c24276277004f33f2a
rayhanjofan <rayhanjofan21@gmail.com>
2025-11-19 12:50:48 +0700

    Percobaan 3

---
+++ Jobsheet10/Numbers24.java
@@ -0,0 +1,17 @@
+package Jobsheet10;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Numbers24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] myNumbers = new int[3][];
+
+        myNumbers[0] = new int[5];
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
+        
+    }
+}
