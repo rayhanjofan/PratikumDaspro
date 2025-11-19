2125681209611824682def8fc6e8ea496bbf52a4
rayhanjofan <rayhanjofan21@gmail.com>
2025-11-19 12:51:21 +0700

    Modifikasi Percobaan 3

--- Jobsheet10/Numbers24.java
+++ Jobsheet10/Numbers24.java
@@ -13,5 +13,11 @@ public class Numbers24 {
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
         
+        for (int i = 0; i < myNumbers.length; i++)
+        System.out.println(Arrays.toString(myNumbers[i]));
+    
+        for (int i = 0; i < myNumbers.length; i++) {
+        System.out.println("Panjang baris ke- " + (i+1) + " : " + myNumbers[i].length);
+}
     }
 }
