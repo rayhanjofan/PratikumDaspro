2397002a79d7582289032fd5803366c2b9c80367
rayhanjofan <rayhanjofan21@gmail.com>
2025-11-28 16:10:52 +0700

    Percobaan 1

---
+++ Jobsheeet11/Kafe24.java
@@ -0,0 +1,17 @@
+package Jobsheeet11;
+
+import java.util.Scanner;
+
+public class Kafe24 {
+    public static void Menu() {
+        System.out.println("=== DAFTAR MENU KAFE ===");
+        System.out.println("1. Nasi Goreng - 15000");
+        System.out.println("2. Mie Ayam    - 12000");
+        System.out.println("3. Es Teh      -  5000");
+        System.out.println("4. Kopi        - 10000");
+        System.out.println("========================");
+    }
+    public static void main(String[] args) {
+        Menu();
+    }
+}
