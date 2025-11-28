cb2b15a361b4b73d90a1552db21759d09e8ed54a
rayhanjofan <rayhanjofan21@gmail.com>
2025-11-28 16:35:00 +0700

    Percobaan 2

--- Jobsheeet11/Kafe24.java
+++ Jobsheeet11/Kafe24.java
@@ -3,7 +3,13 @@ package Jobsheeet11;
 import java.util.Scanner;
 
 public class Kafe24 {
-    public static void Menu() {
+    static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - 15,000");
         System.out.println("2. Cappuccino - 20,000");
@@ -14,7 +20,9 @@ public class Kafe24 {
         System.out.println("========================");
         System.out.println("Silahkan pilih menu yang anda inginkan.");
     }
+
     public static void main(String[] args) {
-        Menu();
+        Menu("Andi", true);
     }
 }
+
