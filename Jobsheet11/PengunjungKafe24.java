0c585d8b06ee2014c897303767d619937b8571b3
rayhanjofan <rayhanjofan21@gmail.com>
2025-11-29 15:39:15 +0700

    Modifikasi Percobaan 4

--- Jobsheet11/PengunjungKafe24.java
+++ Jobsheet11/PengunjungKafe24.java
@@ -5,8 +5,8 @@ public class PengunjungKafe24 {
     static void daftarPengunjung(String...namaPengunjung) {
 
         System.out.println("Daftar Nama pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++){
-            System.out.println("- " + namaPengunjung[i]);
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
         }
     }
     public static void main(String[] args) {
