fbcd6abf9a8493e85f0fd39e6af1bc0be399b9c3
rayhanjofan <rayhanjofan21@gmail.com>
2025-12-07 11:15:39 +0700

    Tugas 2

---
+++ Jobsheet11/NilaiMahasiswa24.java
@@ -0,0 +1,43 @@
+package Jobsheet11;
+
+import java.util.Scanner;
+
+public class NilaiMahasiswa24 {
+
+    static void isianArray(int[] arr) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+    }
+    static void tampilArray(int[] arr) {
+        System.out.println("\n=== Daftar Nilai Mahasiswa ===");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Nilai mahasiswa ke-" + (i + 1) + ": " + arr[i]);
+        }
+    }
+    static int hitTot(int[] arr) {
+        int total = 0;
+        for (int nilai : arr) {
+            total += nilai;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah mahasiswa (N): ");
+        int N = sc.nextInt();
+
+        int[] nilaiMhs = new int[N];
+
+        isianArray(nilaiMhs);
+        tampilArray(nilaiMhs);
+
+        int totalNilai = hitTot(nilaiMhs);
+        System.out.println("\nTotal nilai seluruh mahasiswa = " + totalNilai);
+    }
+}
